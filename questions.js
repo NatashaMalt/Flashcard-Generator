4ac27f4e817f3178a05dabd98f80b166dfbcb020
Natasha Malt <natashamalt@Natashas-MacBook-Air.local>
2017-04-14 20:39:59 -0700

    finished

--- questions.js
+++ questions.js
@@ -1,17 +1,42 @@
 exports.questions = [
 	{
-		full: 'The technical term for a cat’s hairball is a “bezoar"',
+		full: 'The technical term for a cat’s hairball is a “bezoar".',
 		cloze: 'bezoar'
 	},
 
 	{ full: 'A cat can travel at a top speed of approximately 31 mph (49 km).',
 	  cloze: '31 mph'
+	},
 
-    },
-
-    { full:'A cat lover is called an "Ailurophilia"',
+    { full:'A cat lover is called an "Ailurophilia".',
       cloze: 'Ailurophilia' 
     },
+
+    { full:'A group of cats is called a "clowder".',
+      cloze: 'clowder'
+
+    },
+
+    { full:'Cats have 230 bones.',
+      cloze: '230' 
+
+    },
+
+    { full: 'The earliest evidence of feline domestication is from 9,500 years ago.',
+      close: '9,500'
+
+    },
+    
+    { full: 'Cats generally have about 12 whiskers on each side of their face.',
+      cloze: '12'
+     },
+
+     { full: 'Cats always make purrfect friends.',
+       cloze: 'purrfect'
+
+     }
+    
+    
     
 	
 	

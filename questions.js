cc0319b864dcbf8a6112ade1bba48fc0098ae5ba
Natasha Malt <natashamalt@Natashas-MacBook-Air.local>
2017-04-14 20:06:41 -0700

    flascards

---
+++ questions.js
@@ -0,0 +1,18 @@
+exports.questions = [
+	{
+		full: 'The technical term for a cat’s hairball is a “bezoar"',
+		cloze: 'bezoar'
+	},
+
+	{ full: 'A cat can travel at a top speed of approximately 31 mph (49 km).',
+	  cloze: '31 mph'
+
+    },
+
+    { full:'A cat lover is called an "Ailurophilia"',
+      cloze: 'Ailurophilia' 
+    },
+    
+	
+	
+]
